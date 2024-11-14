18b3355069edb55501a0a7cb070078be2ba75be1
Chetan Dange <dangechetan3@gamil.com>
2024-11-15 00:03:30 +0530

    movie detail name change

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Popular from "./Components/Popular"
 import Movie from "./Components/Movie"
 import Tvshows from './Components/Tvshows';
 import People from './Components/People';
-import Moviedetails from './Components/moviedetails';
+import Moviedetails from './Components/Moviedetails';
 import TvDetails from './Components/TvDetails';
 import PersonDetails from './Components/PersonDetails';
 import Trailer from './Components/partials/Trailer';
