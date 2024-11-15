2cee1960135697548afeda83db96d7182c2c4655
Chetan Dange <dangechetan3@gamil.com>
2024-11-15 19:50:24 +0530

    new

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React from 'react';
 
 const instance = axios.create({
-    baseURL:"http://api.themoviedb.org/3/",
+    baseURL:"https://api.themoviedb.org/3/",
     headers: {
         accept: 'application/json',
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMGU1ZGJhNzM3M2UyZDlhNDM4YWQ1MzIyOGNhNTNkYiIsIm5iZiI6MTczMTI2NDc2Ni43MzQ4NTQ1LCJzdWIiOiI2NzMwZTdlZTU3YjMwMWYzYzkzMWMwZTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Zeuqzlwf3u93akbrmW7nE4YyK17SDo-uKRGgcNEQKGg'
